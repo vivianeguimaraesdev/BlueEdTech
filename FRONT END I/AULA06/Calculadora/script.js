4ff715f2610c06685895e38f2bdeb3440bca52dd
Viviane Guimarães <83373229+vivianeguimaraesdev@users.noreply.github.com>
2021-07-20 18:01:41 -0300

    Update script.js

--- FRONT END I/AULA06/Calculadora/script.js
+++ FRONT END I/AULA06/Calculadora/script.js
@@ -1,113 +1,294 @@
-/*Pegando o elemento do displayp pelo ID*/
-
+/**
+ * Here we get the element display where we're going to see the calculations
+ */
 let display = document.getElementById("display-screen");
 
-let esquerda;
-let direita;
-let indexDaOperacao;
-let calculoFinal;
-//Objeto - Operações
-let operacoes = {
-    soma:false,
-    multiplicacao: false,
-    divisao: false,
-    subtracao: false,
-    potencia: false,
-    raiz: false,    
+/**
+ * Now we declare some variables to store the first part of the expression, the second, the operation and the calculation end.
+ */
+let temp1;
+let temp2;
+let opIndex;
+let operation = {
+  sum: false,
+  plus: false,
+  divide: false,
+  minus: false,
+  pow: false,
+  sqrt: false,
+  percent: false,
+  parenthesis: false,
+  parenthesisIndex: []
+}
+let calc;
+let finished = false;
+
+/**
+ * Seven key
+ */
+function typeSeven() {
+  clean();
+  display.value += 7;
 }
 
-//Fazendo os botões
-
-function digitarSete(){
-    limparVisor();
-    display.value = display.value + 7;
+/**
+ * Eight key
+ */
+function typeEight() {
+  clean();
+  display.value += 8;
 }
 
-function digitarOito(){
-    limparVisor();
-    display.value = display.value + 8;
+/**
+ * Nine key
+ */
+function typeNine() {
+  clean();
+  display.value += 9;
 }
 
-function digitarNove(){
-    limparVisor();
-    display.value = display.value + 9;
+/**
+ * Four key
+ */
+ function typeFour() {
+  clean();
+  display.value += 4;
 }
 
-function digitarQuatro(){
-    limparVisor();
-    display.value = display.value + 4;
+/**
+ * Five key
+ */
+ function typeFive() {
+  clean();
+  display.value += 5;
 }
 
-function digitarCinco(){
-    limparVisor();
-    display.value = display.value + 5;
+/**
+ * Six key
+ */
+ function typeSix() {
+  clean();
+  display.value += 6;
 }
 
-function digitarSeis(){
-    limparVisor();
-    display.value = display.value + 6;
+/**
+ * One key
+ */
+ function typeOne() {
+  clean();
+  display.value += 1;
 }
 
-
-function digitarUm(){
-    limparVisor();
-    display.value = display.value + 1;
+/**
+ * Two key
+ */
+ function typeTwo() {
+  clean();
+  display.value += 2;
 }
 
-function digitarDois(){
-    limparVisor();
-    display.value = display.value + 2;
+/**
+ * Three key
+ */
+ function typeThree() {
+  clean();
+  display.value += 3;
 }
 
-function digitarTrês(){
-    limparVisor();
-    display.value = display.value + 3;
+/**
+ * Zero key
+ */
+ function typeZero() {
+  clean();
+  display.value += 0;
 }
 
-function digitarZero(){
-    limparVisor();
-    display.value = display.value + 0;
+/**
+ * Point key
+ */
+ function typePoint() {
+  clean();
+  display.value += ".";
 }
 
-//Operações
-
-function digitarDivisao(){
-    limparVisor();
-    display.value = display.value + '÷';
-    operacoes.divisao = true;
+/**
+ * Operations
+ * Divide key
+ */
+function typeDivide() {
+  clean();
+  display.value += "÷";
+  operation.divide = true;
 }
 
-function digitarMultiplicacao(){
-    limparVisor();
-    display.value = display.value + 'x';
-    operacoes.multiplicacao = true;
+/**
+ * Plus key
+ */
+function typePlus() {
+  clean();
+  display.value += "x";
+  operation.plus = true;
 }
 
-function digitarSubstracao(){
-    limparVisor();
-    display.value = display.value + '-';
-    operacoes.subtracao = true;
+/**
+ * Sum key
+ */
+ function typeSum() {
+  clean();
+  display.value += "+";
+  operation.sum = true;
 }
 
-function digitarSoma(){
-    limparVisor();
-    display.value = display.value + '+';
-    operacoes.soma = true;
+/**
+ * Sum minus
+ */
+ function typeMinus() {
+  clean();
+  display.value += "-";
+  operation.minus = true;
 }
 
-
-
-
-/* Limpar o display quando digitar o primeiro número */
-
-function limparVisor(){
-    if (display.value == 0){
-        display.value = " ";
-    }
+/**
+ * Percent key
+ */
+ function typePercent() {
+  clean();
+  display.value += '%';
+  operation.percent = true;
 }
 
-function calcular(operacao){
-    posicaoDaOperacao = display.value.indexOf(operacao);
-    esquerda = parseInt(display.value.substring(0, posicaoDaOperacao));
-    direita = display.value.substring(posicaoDaOperacao + 1, display.value.length)
+/**
+ * Sqrt key
+ */
+ function typeSqrt() {
+  clean();
+  display.value += '√';
+  operation.sqrt = true;
 }
+
+/**
+ * Pow key
+ */
+ function typePow() {
+  clean();
+  display.value += "^";
+  operation.pow = true;
+}
+
+/**
+ * Left parenthesis key
+ */
+ function typeLeftPar() {
+  clean();
+  display.value += "(";
+  operation.parenthesis = true;
+  operation.parenthesisIndex.push(display.value.indexOf("("));
+  console.log(operation.parenthesisIndex);
+}
+
+/**
+ * Left parenthesis key
+ */
+ function typeRightPar() {
+  clean();
+  display.value += ")";
+  operation.parenthesis = true;
+  operation.parenthesisIndex.push(display.value.indexOf(")"));
+  console.log(operation.parenthesisIndex);
+}
+
+/**
+ * When the equals is pressed we verify the operation and get the substring of left and right and after 
+ */
+function calculate() {
+
+  if(operation.divide) {
+    simpleOp("÷");
+  }
+  else if(operation.minus) {
+    simpleOp("-");
+  }
+  else if(operation.sum) {
+    simpleOp("+");
+  }
+  else if(operation.plus) {
+    simpleOp("x");
+  }
+  else if(operation.pow) {
+    simpleOp('^');
+  }
+  else if(operation.pow) {
+    simpleOp('√');
+  }
+  else {
+    display.value = 'ERROR';
+  }
+}
+
+/**
+ * Erase the last character of the string
+ */
+function erase() {
+  if(display.value != 0) {
+    display.value = display.value.slice(0, display.value.length - 1);
+  }
+}
+
+/**
+ * Clear the display addng a zero value and also reinitilizing all variables
+ */
+function clearDisplay() {
+  display.value = 0;
+  temp1 = 0;
+  temp2 = 0;
+  opIndex = 0;
+  operation = {
+    sum: false,
+    plus: false,
+    divide: false,
+    minus: false,
+    pow: false,
+    sqrt: false,
+    percent: false,
+    parenthesis: false,
+    parenthesisIndex: []
+  }
+}
+
+/**
+ * When typping a new number, reset the Display
+ */
+function clean() {
+  if(display.value == 0) {
+    display.value = "";
+  }
+}
+
+/**
+ * Simple operation
+ */
+function simpleOp(operation) {
+  if(operation == '√') {
+    opIndex = 0;
+    temp1 = parseInt(display.value.substring(opIndex + 1, display.value.length));
+
+  } else {
+    opIndex = display.value.indexOf(operation);
+    temp1 = parseInt(display.value.substring(0, opIndex));
+    temp2 = parseInt(display.value.substring(opIndex + 1, display.value.length));
+  }
+  
+  if(operation == '+') {calc = temp1 + temp2}
+  else if(operation == '-') {calc = temp1 - temp2}
+  else if(operation == '*') {calc = temp1 * temp2}
+  else if(operation == '÷') {calc = temp1 / temp2}
+  else if(operation == '^') {calc = Math.pow(temp1, temp2)}
+  else if(operation == '√') {calc = Math.sqrt(temp1)}
+  
+  if (isNaN(calc)) {
+    display.value = 'ERROR';
+  } else {
+    display.value = calc;
+  }
+}
+ 
